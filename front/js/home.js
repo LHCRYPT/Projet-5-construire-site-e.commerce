2de23c24d0290204d96331aa4ac5c7b25f64ca0a
LHCRYPT <l.hamadouche@laposte.net>
2021-11-16 18:39:14 +0100

    js photos

--- front/js/home.js
+++ front/js/home.js
@@ -8,3 +8,17 @@ let nom='produit.name';
 clone.querySelector('.productName').innerHTML=nom;
 items.appendChild(clone);
 
+//plan qui détaille le produit nom des produits et leurs prix. 
+//On met un P majuscule car c'est un objet
+function Products(pic,name,price){
+    this.pic = pic,
+    this.name = name,
+    this.price = price
+}
+
+//array tableau pour enregistrer une liste de produits
+let products = [];
+
+products.push(product,product2);
+
+console.log(products[1].name);
