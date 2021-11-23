4577085ba9cb97b3903252efdd35dd07dbd8debf
LHCRYPT <l.hamadouche@laposte.net>
2021-11-23 21:53:44 +0100

    création de product js pour la 2ème page du site pour indiquer les détails des produits

--- front/js/home.js
+++ front/js/home.js
@@ -13,73 +13,11 @@ fetch(url)
     clone.querySelector('.imgName').setAttribute('src',p.imageUrl);
    
     clone.querySelector('.productDescription').innerHTML=p.description;
+    let href = './product.html?id='+p._id;
+    clone.querySelector('.lien').setAttribute('href',href);
     items.appendChild(clone);
 }
 });
-/* Tu reçois qu'un objet et non un tableau d'objet
-Donc pas besoin de foreach! tu peux directement utiliser un element dans ton html
-const recover = (elements) => {
-    elements.forEach(element => {
-        console.log(element);
-        document.querySelector(".row").innerHTML +=
-        <div class="col">
-            <div class="card align-self-start" style="width: 18rem;">
-                <img width="100%" src=' §{element.imageUrl} '>
-                    <div class="card-body">
-                        <p> §{element.name} </p> ><p> §{element.price/100} euros </p> §{element.description} </p>
-                    </div>
-            </div>
-        </div>
-    });
-}*/
-
-/*let template=document.querySelector('#produit');
-let items=document.querySelector('#items');
-// Clone the new row and insert it into the table
-let clone = template.content.cloneNode(true);
-//je viens alimenter la div items avec le template
-let nom='produit.name';
-clone.querySelector('.productName').innerHTML=nom;
-items.appendChild(clone);
-
-//plan qui détaille le produit nom des produits et leurs prix. 
-//On met un P majuscule car c'est un objet
-function Products(pic,name,price){
-    this.pic = pic,
-    this.name = name,
-    this.price = price
-}
-
-const product1 = new Products('images/kanap01.jpeg', 'canapé bleu', 70);
-
-//array tableau pour enregistrer une liste de produits
-let products = [];
-
-products.push(product1,product2);
-
-console.log(products[1].pic); */
 
 
-//plan qui détaille le produit nom des produits et leurs prix
 
-
-let product = {
-
-    name : "kanap01",
-    price : 55
-};
- 
-let product2 = {
-
-    name : "kanap01",
-    price : 65
-
-};
-
-//array tableau pour enregistrer une liste de produits
-
-let products = [];
-
-products.push(product,product2);
-
-console.log(products[1].name);
