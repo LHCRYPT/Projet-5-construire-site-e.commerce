63b2cdf2cc8de5853659626fda7c26f40486d469
LHCRYPT <l.hamadouche@laposte.net>
2021-11-26 17:55:02 +0100

    création 2 fichiers js:confirmartion et cart

---
+++ front/js/cart.js
@@ -0,0 +1,18 @@
+	//mon url = product.html?id=1234
+	// object let id = 1234
+	//On recupere le chemin complet
+	let chemin = window.location.href;
+	// on decoupe la chaine de caractere en tableau au du =
+	let tableau = chemin.split('=');
+	// tableau[0] = 'product.html?id'
+	// tableau[1] = '1234'
+	let id = tableau[1];
+	console.log(id);
+
+	// recupéré le produit
+	let url ='http://localhost:3000/api/products/'+id; // on concatene l id a la chaine de caracteres
+
+fetch(url)
+.then(response=>response.json())
+.then(product=>{
+     console.log(product);
