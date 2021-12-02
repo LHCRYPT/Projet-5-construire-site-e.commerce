5c9b85903d9632dedfbed0ee1b03f4840570ecaa
LHCRYPT <l.hamadouche@laposte.net>
2021-12-02 19:38:38 +0100

    création du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,1 +1,42 @@
+let url = "http://localhost:3000/api/products/";
+fetch(url)
+.then(response=>response.json())
+.then(data=>{
+    for(let p of data){ 
+    let template=document.querySelector('#produit');
+    let items=document.querySelector('#items');
+    // Clone the new row and insert it into the table
+    let clone = template.content.cloneNode(true);
+    //je viens alimenter la div items avec le template
+    let nom=p.name;
+    clone.querySelector('.productName').innerHTML=nom;
+    clone.querySelector('.imgName').setAttribute('src',p.imageUrl);
+   
+    clone.querySelector('.productDescription').innerHTML=p.description;
+    let href = './product.html?id='+p._id;
+    clone.querySelector('.lien').setAttribute('href',href);
+    items.appendChild(clone);
+}
+});
+
 //récupérer le tableau depuis local storage et l'afficher avec boucle for each
+//lire local storage
+
+let panier = [];
+	// verifier s'il y a qlq chose
+	if (localStorage.panier != undefined)
+	{
+		let str = localStorage.tableau;
+		// on transforme la chaine de caractere en VRAI array ou tableau
+		panier = JSON.parse(str);
+	}
+
+let tableau   
+	// for each  pour déplier ou afficher le tableau
+	for ( let color of tableau)
+	{
+		console.log(color);
+
+ }
+
+ //cibler les balises pour créer tableau html
--- front/js/product.js
+++ front/js/product.js
@@ -11,12 +11,13 @@
 
 	// recupéré le produit
 	let url ='http://localhost:3000/api/products/'+id; // on concatene l id a la chaine de caracteres
-let id; //déclaration globale : pour que les id restent mémoriser, donc je le mets en dehors de la boucle then
+let produit; //on déclare produit en global, pour pouvoir le réutiliser par la suite
 fetch(url)
 .then(response=>response.json())
 .then(product=>{
      console.log(product);
-     id = product._id;
+     produit = product; //mémorise le produit
+    
     document.querySelector('#title').innerHTML=product.name;
     document.querySelector('#price').innerHTML=product.price;
     document.querySelector('#image').setAttribute('src',product.imageUrl);
@@ -38,74 +39,33 @@ fetch(url)
 document.querySelector('#addToCart').onclick = function()
 {
     //récupérer couleur nom etc
-let color = document.querySelector('#color').value;
-console.log(id);
-console.log(color);
+    let color = document.querySelector('#color').value;
+    console.log(id);
+    console.log(color);
+    console.log(produit);
+    let quantity = document.querySelector('#itemQuantity').value;
+    console.log(quantity);
 
 
-// créer un panier
-// on stocke
-let panier = ['rouge','bleu','violet'];
-console.log(panier);
-// on transforme en chaine de carac.
-let str = JSON.stringify(panier)
-console.log(str);
-//localStorage.toto .. = ... 
-localStorage.tableau = str;
+    let line = {};  //je crée un objet une ligne de commande;
+	// attributs ce sont les variables d'un  objet
+	line.product = produit; // je definie un attribut non et sa valeur
+	line.color = color;
+	line.quantity = quantity;
+  
 
-let panier = [];
-	// verifier si il y a qlq chose
-	if (localStorage != undefined)
+    let panier = [];
+	// verifier s'il y a qlq chose
+	if (localStorage.panier != undefined)
 	{
 		let str = localStorage.tableau;
-		// on transforme la chaine de caractere en VRAI array ou tablea
+		// on transforme la chaine de caractere en VRAI array ou tableau
 		panier = JSON.parse(str);
 	}
-	console.log(panier);
-
-//faire un push dans un tableau : produit couleur prix quantité
-let tableau = ['Kanap sinopé','bleu','1849', '1'];
-	console.log(tableau);
-	
- let tableau   
-	// for each  pour déplier ou afficher le tableau
-	for ( let color of tableau)
-	{
-		console.log(color);
-
- 
-
-
-}
-let personne = {};  //je crée un objet personne;
-	// attributs ce sont les variables d'un  obejet
-	personne.nom = 'MOI'; // je definie un attribut non et sa valeur
-	personne.prenom = 'Linda';
-	personne.adresse = '32 rue du Paradis' ;
-    personne.ville = 'grenoble';
-    personne.email = 'linda.moi@toujours.fr';
-
-
-	tabPersonnes.push(personne);
-
-	let p ={ nom:'MOI',prenom:'Linda',adresse:'32 rue du Paradis' ,ville:'grenoble' ,email: 'linda.moi@toujours.fr' };
-	tabPersonnes.push(p);
-
-	console.log(tabPersonnes);
-
-	// afficher les personnes 
-	for ( let people of tabPersonnes)
-	{
-		console.log(people.prenom+' '+people.nom);
-		let demo = document.querySelector('#demo');
-		let p = document.createElement('p'); // <p></p>
-		p.innerHTML = people.prenom+' '+people.nom; // <p> Linda MOI</p>
-		// je met p dans la div
-		demo.appendChild(p);
-	}
-//puis stocké sur local storage ce tableau
-
-
- //window.location = 'cart.html'; //redirige vers la page html
-
-
+    panier.push(line);
+    	// on transforme en chaine de carac.
+	let str = JSON.stringify(panier)
+	console.log(str);
+	//localStorage.t = ... 
+	localStorage.panier = str;
+}    
