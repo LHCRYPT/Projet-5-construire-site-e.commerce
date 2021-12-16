3f5d00eb5e2cdd78b5f9ee67a97a66c9d260ba6a
LHCRYPT <l.hamadouche@laposte.net>
2021-12-16 20:39:09 +0100

    enregistrement dans le local storage

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,25 +7,51 @@ let panier = [];
 		panier = JSON.parse(str);
 	}
 
-  let emplacement = document.querySelector('#cart__items'); // sélecteur sur la zone de dépôt
-  let template = document.querySelector('#template'); //sélecteur sur le template
+     //afficher le panier
+     function afficherPanier () {
 
+     
+  let emplacement = document.querySelector('#cart__items'); // sélecteur sur la zone de dépôt
+  emplacement.innerHTML = '';
+  let template = document.querySelector('#template'); //sélecteur sur le template
+  let indice = 0;
 	// for each  pour déplier ou afficher le tableau
 	for ( let line of panier)
 	{
 		console.log(line); 
+        
         // on clone le template
         let clone = template.content.cloneNode(true);
         //on ajoute les infos de l'article : quantité, couleur etc A FAIRE 
         clone.querySelector('h2').innerHTML = line.product.name;
         clone.querySelector('img').setAttribute('src', line.product.imageUrl); // <img src="code ici"
 
-        //afficher le prix, l'id, quantité + total
-        clone.querySelector('.prix').innerHTML = line.product.price;
-        clone.querySelector('.itemQuantity').innerHTML = line.product.quantity;
-        //clone.querySelector('.total').innerHTML = line.product.total;
+        //afficher le prix, quantité + total
+        clone.querySelector('.prix').innerHTML = line.product.price +' euros';
         // multiplier le prix par la quantité
+
+        clone.querySelector('.color').innerHTML = line.color;
+        clone.querySelector('.itemQuantity').value = line.quantity;
+        //on stocke l'indice dans un attribut sur le bouton, je veus savoir sur quelle ligne je suis
+        clone.querySelector('.deleteItem').setAttribute ('data-indice', indice);
+        clone.querySelector('.deleteItem').onclick = function (){
+        let i = this.getAttribute('data-indice');
+            console.log (i); //on supprime la ligne i (indice)
+            panier.splice(i, 1);
+            afficherPanier();
+            //enregistrer dans le local storage
+            localStorage.panier = JSON.stringify(panier);
+        } 
+        
+        indice++;
+       
+        
+
         // on ajoute le clone à la zone de dépôt/emplacement
         emplacement.appendChild(clone);
 
  }
+ //grand total faire une variable total et la mettre à 0
+document.getElementById innerHTML
+} 
+afficherPanier ();
