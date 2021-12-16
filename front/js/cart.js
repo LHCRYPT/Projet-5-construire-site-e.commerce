785a2d96540daa9ee8e5edc15caec8ded1637daa
LHCRYPT <l.hamadouche@laposte.net>
2021-12-16 19:00:24 +0100

    affichage prix quantité

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,37 +1,3 @@
-/*
-let url = "http://localhost:3000/api/products/";
-fetch(url)
-.then(response=>response.json())
-.then(data=>{
-    for(let p of data){ 
-    let template=document.querySelector('#produit');
-    let items=document.querySelector('#items');
-    // Clone the new row and insert it into the table
-    let clone = template.content.cloneNode(true);
-    //je viens alimenter la div items avec le template
-    let nom=p.name;
-    clone.querySelector('.productName').innerHTML=nom;
-    clone.querySelector('.imgName').setAttribute('src',p.imageUrl);
-   
-    clone.querySelector('.productDescription').innerHTML=p.description;
-    let href = './product.html?id='+p._id;
-    clone.querySelector('.lien').setAttribute('href',href);
-    items.appendChild(clone);
-}
-});
-
-	//On recupere le chemin complet
-	let chemin = window.location.href;
-	// on decoupe la chaine de caractere en tableau au du =
-	let tableau = chemin.split('=');
-	// tableau[0] = 'cart.html?id'
-	// tableau[1] = '1234'
-	let id = tableau[1];
-	console.log(id);
-*/
-//récupérer le tableau depuis local storage et l'afficher avec boucle for each
-//lire local storage
-
 let panier = [];
 	// verifier s'il y a qlq chose
 	if (localStorage.panier != undefined)
@@ -53,9 +19,11 @@ let panier = [];
         //on ajoute les infos de l'article : quantité, couleur etc A FAIRE 
         clone.querySelector('h2').innerHTML = line.product.name;
         clone.querySelector('img').setAttribute('src', line.product.imageUrl); // <img src="code ici"
-        
+
         //afficher le prix, l'id, quantité + total
         clone.querySelector('.prix').innerHTML = line.product.price;
+        clone.querySelector('.itemQuantity').innerHTML = line.product.quantity;
+        //clone.querySelector('.total').innerHTML = line.product.total;
         // multiplier le prix par la quantité
         // on ajoute le clone à la zone de dépôt/emplacement
         emplacement.appendChild(clone);
