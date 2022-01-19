7cb06c63d7e29db31b41e2719f4fedf67f54e895
LHCRYPT <l.hamadouche@laposte.net>
2022-01-19 23:10:02 +0100

    récup localstorage

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,10 @@
 //récupérer n° local storage FAIT PAR MOI
 function allStorage() {
-    
+
 }
+
+let product = JSON.parse(localStorage.getItem("product"));
+console.log(product);
+const panierDisplay = async () => {
+    console.log(salut);
+} 
