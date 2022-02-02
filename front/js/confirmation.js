e2322f0a3a62c5533395a76d556ae6100b39619b
LHCRYPT <l.hamadouche@laposte.net>
2022-02-03 00:00:23 +0100

    réupération n° commande

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,5 +1,6 @@
 //récupérer n° local storage FAIT PAR MOI
-
+//enregistrer dans le local storage
+localStorage.panier = JSON.stringify(panier);
 //let numCommande =
 
-var retrievedObject = localStorage.getItem('added-items');
+var retrievedObject = localStorage.getItem('panier-items');
